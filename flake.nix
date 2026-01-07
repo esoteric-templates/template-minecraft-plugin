e59ba475932671e23d3a004b6decb8b2f4f9e3bf
Дима Ш. <dima.o.sh@proton.me>
2026-01-07 10:20:17 +0000

    Update nixpkgs channels

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
 	description = "A template repository for Kotlin projects";
 
 	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 		flake-utils.url = "github:numtide/flake-utils";
 	};
 
